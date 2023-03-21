bf9cdb441fdd8aef61bb393a35ce791a7a402a2b
RootCellar <RootCellar9877@GMail.com>
2023-03-21 13:34:40 -0800

    update zram.sh to be more clear / easy to read

--- zram.sh
+++ zram.sh
@@ -1,7 +1,9 @@
-#su root
+#!/bin/bash
+
+#Run as root
 
 modprobe zram
 
-echo 8192M > /sys/block/zram0/disksize
+echo 8G > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
 swapon -p 100 /dev/zram0
