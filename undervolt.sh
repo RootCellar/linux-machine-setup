b99ebbf75f357fb3776454730f9f9e8dacac7725
RootCellar <RootCellar9877@GMail.com>
2023-03-21 13:44:14 -0800

    add comments to undervolt.sh

--- undervolt.sh
+++ undervolt.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+
+# This is more likely to be useful at home,
+# but anyhow this reduces the voltage to each part of the CPU.
+# This only works on intel as far as I know,
+# and you need to figure out good values for these as making them too high will freeze up your system.
+#
+# The '-t' option sets the maximum operating temperature in Celsius, 
+# handy if you prefer thermal throttling to happen sooner.
+#
+# Note: 'core' and 'cache' need to be the same.
+
+
 sudo undervolt --core -120 --cache -120 --gpu -100 -t 85
 sudo undervolt --read
