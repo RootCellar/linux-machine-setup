36d50596904bac9bfebc9b21f0594923c1183b08
RootCellar <RootCellar9877@GMail.com>
2023-03-21 13:34:30 -0800

    add shebang

--- ubuntuinstalls.sh
+++ ubuntuinstalls.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update
 
 sudo apt install -y rsync wget gzip git gcc g++ htop neofetch make build-essential net-tools lm-sensors cpufrequtils tcpdump docker docker-compose screen zip unzip telnet lshw python3 python-is-python3 sysstat zram-tools
