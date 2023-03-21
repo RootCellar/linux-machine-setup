74aa70ebe25095f564c89545da9a266554d4c5f5
RootCellar <RootCellar9877@GMail.com>
2023-03-21 13:40:17 -0800

    add optional command to zram.sh

--- zram.sh
+++ zram.sh
@@ -7,3 +7,6 @@ modprobe zram
 echo 8G > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
 swapon -p 100 /dev/zram0
+
+# Please only uncomment any of the below if you want it and know what you are doing!
+# sysctl vm.swappiness=100 # Makes the kernel more likely to swap memory pages out to zram or disk in favor of file caches
