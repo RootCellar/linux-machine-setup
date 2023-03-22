0b7e106343e9ede582f4f68fa9fcd6b9ba5933f9
RootCellar <RootCellar9877@GMail.com>
2023-03-21 16:30:52 -0800

    clarify this command

--- zram.sh
+++ zram.sh
@@ -9,4 +9,4 @@ mkswap --label zram0 /dev/zram0
 swapon -p 100 /dev/zram0
 
 # Please only uncomment any of the below if you want it and know what you are doing!
-# sysctl vm.swappiness=100 # Makes the kernel more likely to swap memory pages out to zram or disk in favor of file caches
+# sysctl vm.swappiness=100 # Makes the kernel more likely to swap memory pages out to zram or disk in favor of file caches. Does not persist on shutown/reboot
