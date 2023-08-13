5f4a5504e633722bae35aee481bcfbf8b75d2a5b
RootCellar <RootCellar9877@gmail.com>
2023-08-12 22:56:04 -0800

    remove linux-tools-common, does not seem to be a thing anymore

--- ubuntuinstalls.sh
+++ ubuntuinstalls.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 
-sudo apt install -y linux-tools-common openssh-server openssh-sftp-server rsync wget gzip git gcc g++ htop neofetch make build-essential whois dnsutils net-tools lm-sensors cpufrequtils tcpdump docker docker-compose screen zip unzip telnet lshw python3 python-is-python3 sysstat zram-tools
+sudo apt install -y openssh-server openssh-sftp-server rsync wget gzip git gcc g++ htop neofetch make build-essential whois dnsutils net-tools lm-sensors cpufrequtils tcpdump docker docker-compose screen zip unzip telnet lshw python3 python-is-python3 sysstat zram-tools
 
 # This may need to be run on ubuntu server VMs because they don't come with all kernel modules like Zram: sudo apt install linux-image-extra-virtual
