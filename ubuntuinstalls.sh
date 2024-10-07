4d9048701458d37a693e0644db8dddcf2c5fe2ca
RootCellar <RootCellar9877@gmail.com>
2024-10-06 20:13:44 -0800

    clean up installs

--- ubuntuinstalls.sh
+++ ubuntuinstalls.sh
@@ -2,6 +2,11 @@
 
 sudo apt update
 
-sudo apt install -y openssh-server openssh-sftp-server rsync wget gzip git gcc g++ htop neofetch make build-essential whois dnsutils net-tools lm-sensors cpufrequtils tcpdump docker docker-compose screen zip unzip telnet lshw python3 python-is-python3 sysstat zram-tools stress mpich
+sudo apt install -y openssh-server rsync wget gzip git gcc g++ htop neofetch make \
+build-essential whois dnsutils net-tools lm-sensors cpufrequtils tcpdump screen zip unzip telnet \
+lshw python3 python-is-python3 sysstat stress smartmontools ufw
 
-# This may need to be run on ubuntu server VMs because they don't come with all kernel modules like Zram: sudo apt install linux-image-extra-virtual
+# extras: mpich zram-tools docker docker-compose
+
+# This may need to be run on ubuntu server VMs because they don't come with all kernel modules like Zram: 
+# sudo apt install linux-image-extra-virtual
