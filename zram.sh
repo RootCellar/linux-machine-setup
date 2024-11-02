7429e542dff3e12eb9f43a23996e92ae52ec99ae
RootCellar <RootCellar9877@gmail.com>
2024-11-02 13:42:30 -0800

    improve zram.sh with comments and basic error handling

--- zram.sh
+++ zram.sh
@@ -1,14 +1,49 @@
-#!/bin/bash
+#!/bin/env bash
 
-#Run as root
+# Written by RootCellar
+# rootcellar on Discord, RootCellar on Github
+#
+# This script sets up a Zram block device for swap.
+# Before doing so, it first resets the block device to be used.
+# This script can therefore be run at any time to change the size or algorithm of a Zram swap.
+#
+# WARNING: Running this script while memory usage is high may cause problems as the Linux kernel will
+# need to un-swap (and therefore decompress) pages that are currently stored in the Zram block device.
+# Running this script *may* cause your machine to run out of memory in that case. Run at your own risk!
 
-modprobe zram
+# The size of the Zram swap. I would recommend that you set this to a bit less than your system's
+# available RAM.
+# Note: you *can* set this higher than system RAM, and possibly see extended benefits at your own risk.
+SIZE=8G
 
-echo zstd > /sys/block/zram0/comp_algorithm
-# Change the "8G" here to however large you want your zram swap file to be:
-echo 8G > /sys/block/zram0/disksize
-mkswap --label zram0 /dev/zram0
-swapon -p 100 /dev/zram0
+# The compression algorithm to use for the Zram swap.
+# zstd - slowest, however it is the best at compressing data
+# lz4 - fastest, but doesn't save as much space with compression
+# lzo - balance between speed and effectiveness
+#
+# I recommend zstd, despite being slower, because it allows you to get the most out of your system
+# RAM by saving more space than the other algorithms. Plus, by being better at compression,
+# your system won't have to do the swapping and compression as often.
+ALGORITHM=zstd
 
-# Please only uncomment any of the below if you want it and know what you are doing!
-# sysctl vm.swappiness=100 # Makes the kernel more likely to swap memory pages out to zram or disk in favor of file caches. Does not persist on shutown/reboot
+# The block device to use for zram swap
+# This is here because it is possible to have multiple Zram block devices
+# that are used for different purposes. Change this only if you know what you're doing.
+DEVICE=zram0
+
+echo Resetting Zram device...
+
+# Reset Zram Device
+swapoff /dev/$DEVICE || exit 1
+echo 1 > /sys/block/$DEVICE/reset || exit 1
+
+# Setup Zram block device
+echo Initializing Zram device $DEVICE - $SIZE $ALGORITHM ...
+modprobe zram || exit 1
+echo $ALGORITHM > /sys/block/$DEVICE/comp_algorithm || exit 1
+echo $SIZE > /sys/block/$DEVICE/disksize || exit 1
+
+# Enable swap on the zram block device
+mkswap --label $DEVICE /dev/$DEVICE || exit 1
+echo Enabling $DEVICE as swap...
+swapon -p 100 /dev/$DEVICE || exit 1
